6afac78377895372ef769edfa5da55ff6db50351
BayernMuller <cjjunk29@gmail.com>
2020-05-29 11:59:32 +0900

    Add Linux/ file_count.c

---
+++ Linux/file_count/main.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+
+void printCount(char* file)
+{
+	pid_t pid;
+	pid = fork();
+
+	int status;
+	if (!pid)
+	{
+		execlp("wc", "wc", file, NULL);	
+	}
+	waitpid(pid, &status, 0);	
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc == 1)
+	{
+		printf("usage : %s <filename> <filename2> ...)\n", argv[0]);
+		return -1;
+	}
+	
+	char filename[100];
+	for (int i = 1; i < argc; i++)
+	{
+	//	strcpy(filename, argv[i]);
+		printCount(argv[i]);
+	}	
+}
